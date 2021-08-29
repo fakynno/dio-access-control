5a5b7eca37b79c9f372f0165c180f6f550d85956
fakynno <fakynno@gmail.com>
2021-08-29 20:21:27 -0300

    Adicao de classe no model

---
+++ src/main/java/com/dio/live/model/Empresa.java
@@ -0,0 +1,22 @@
+package com.dio.live.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Empresa {
+
+    private long id;
+    private String descricao;
+    private String cnpj;
+    private String endereco;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String telefone;
+}
