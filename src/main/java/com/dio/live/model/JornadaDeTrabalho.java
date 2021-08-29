0fb294c5be8de6b3ac1ed589aa8fce2b217b94f5
fakynno <fakynno@gmail.com>
2021-08-29 19:58:58 -0300

    Inclusao de nova classe no model

---
+++ src/main/java/com/dio/live/model/JornadaDeTrabalho.java
@@ -0,0 +1,15 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class JornadaDeTrabalho {
+
+    private long id;
+    private String descricao;
+}
