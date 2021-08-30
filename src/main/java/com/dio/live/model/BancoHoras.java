7a246c1cdffeed9caff560092f2f3bee8a2a686f
fakynno <fakynno@gmail.com>
2021-08-29 21:32:29 -0300

    Adicao de nova classe no model

---
+++ src/main/java/com/dio/live/model/BancoHoras.java
@@ -0,0 +1,35 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BancoHoras {
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public class BancoHorasId implements Serializable{
+        private long idBancoHoras;
+        private long idMovimento;
+        private long idUsuario;
+    }
+
+    @EmbeddedId
+    private BancoHorasId id;
+    @ManyToOne
+    private CategoriaUsuario categoriaUsuario;
+    private LocalDateTime dataTrabalhada;
+    private BigDecimal qtdHorasTrabalhadas;
+    private BigDecimal saldoHorasTrabalhadas;
+}
