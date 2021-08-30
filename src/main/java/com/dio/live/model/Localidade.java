4a651e11d7c81a89021afde8a61672a27214a29f
fakynno <fakynno@gmail.com>
2021-08-30 00:14:32 -0300

    Add anotacao Entity e Id

--- src/main/java/com/dio/live/model/Localidade.java
+++ src/main/java/com/dio/live/model/Localidade.java
@@ -5,14 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class Localidade {
 
+    @Id
     private long id;
     private String descricao;
 
--- src/main/java/com/dio/live/model/NivelAcesso.java
+++ src/main/java/com/dio/live/model/NivelAcesso.java
@@ -5,12 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class NivelAcesso {
 
+    @Id
     private long id;
     private String descricao;
 }
