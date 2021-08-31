671dbf66e39fefab3d563c95ffe891fcb90a6af8
fakynno <fakynno@gmail.com>
2021-08-31 06:30:18 -0300

    Criacao da Service e metodo save

---
+++ src/main/java/com/dio/live/service/JornadaService.java
@@ -0,0 +1,17 @@
+package com.dio.live.service;
+
+import com.dio.live.model.JornadaDeTrabalho;
+import com.dio.live.repository.JornadaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JornadaService {
+
+    JornadaRepository jornadaRepository;
+
+    public JornadaDeTrabalho save(JornadaDeTrabalho jornadaDeTrabalho) {
+        return jornadaRepository.save(jornadaDeTrabalho);
+    }
+
+
+}
