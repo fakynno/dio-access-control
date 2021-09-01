e6ede10c81153d2e7cdba00489fb75290382f01b
fakynno <fakynno@gmail.com>
2021-08-31 22:52:44 -0300

    Add configs do Swagger

--- src/main/java/com/dio/live/LiveApplication.java
+++ src/main/java/com/dio/live/LiveApplication.java
@@ -2,8 +2,10 @@ package com.dio.live;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class LiveApplication {
 
 	public static void main(String[] args) {
