982a34a8dcd19a33abf97e131239453a00e23a34
fakynno <fakynno@gmail.com>
2021-08-31 21:36:54 -0300

    Add configs do Swagger

--- src/main/java/swagger/SwaggerConfig.java
+++ src/main/java/swagger/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package swagger;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -10,9 +11,12 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
